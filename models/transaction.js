348cae1a97ecee4db26989b3d3f244678d1da5ef
Kathy Gee <mzchick@gmail.com>
2021-08-09 15:30:11 -0400

    remove dollar sign

--- models/transaction.js
+++ models/transaction.js
@@ -2,20 +2,20 @@ module.exports = [
     { 
         date: "June 1",
         name: "Expense",
-        amount: "-$1.00",
+        amount: "-1.00",
         from: "Purchased a Moon Stone",
 
     },
     {
         date: "June 1",
         name: "Expense",
-        amount: "-$10.00",
+        amount: "-10.00",
         from: "Bought Rare Candy",
     },
     {
         date: "June 1",
         name: "Expense",
-        amount: "-$100.00",
+        amount: "-100.00",
         from: "Lost battle to Gary Oak",
     },
   ];
